392d50a33a35df77d6bb5b8b4fb47b89a5e54dbb
agilimam <agilimamalhafizi04@gmail.com>
2023-05-17 16:47:32 +0700

    memanggil program utama

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -31,4 +31,22 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 	nama= iNama;
 }
 
-void mahasiswa::cetak
+void mahasiswa::cetak() {
+	cout << endl << "Nim = " << nim << endl;
+	cout << "Nama = " << nama << endl;
+}
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("Indra");
+	mahasiswa mhs4(30, "Fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+
+}
