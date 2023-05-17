2e86993fd85acc609543d9e0e6a780d168e406d9
agilimam <agilimamalhafizi04@gmail.com>
2023-05-17 16:24:19 +0700

    memberi judul buku dan mengembalikan judulnya

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -4,4 +4,14 @@ using namespace std;
 class buku
 {
 	string judul;
-};
+public:
+	buku setjudul(string judul)
+	{
+		this->judul = judul;
+		return *this; // chain function
+	}
+	string getjudul()
+	{
+		return this->judul;
+	}
+}bukunya;
