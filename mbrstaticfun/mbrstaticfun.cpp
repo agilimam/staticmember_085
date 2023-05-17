4388ac0988dbeb6037749101f82d5226fd9cbac8
agilimam <agilimamalhafizi04@gmail.com>
2023-05-17 17:52:04 +0700

    membuat class mahasiswa privat ada 2 variabel dan public ada 6

---
+++ mbrstaticfun/mbrstaticfun.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+	int id;
+public:
+	string nama;
+
+	void setID();
+	void printAll();
+	
+	static void setNim(int pNim) { nim = pNim; /*Definisi function*/ }
+	static int getNim() { return nim; /*Definisi function*/ }
+	
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+
+
