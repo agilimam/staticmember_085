b5978ad6c0d953143f96fd486944c579aa267f2c
agilimam <agilimamalhafizi04@gmail.com>
2023-05-17 16:44:14 +0700

    mendeklarasi kan mahasiswa nim dengan iNim,nama dengan iNama

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -13,3 +13,22 @@ public:
 	mahasiswa(int iNim, string iNama);
 	void cetak();
 };
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim;
+}
+mahasiswa::mahasiswa(string iNama){
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama= iNama;
+}
+
+void mahasiswa::cetak
